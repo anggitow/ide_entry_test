82f0e2d961a7eedee343631cde12f6b042be36bc
anggitow <anggitov@gmail.com>
2022-11-10 19:27:32 +0700

    Create API getInauguratedSchool

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -14,4 +14,13 @@ class SchoolController extends Controller
         $data = School::whereDate('inaugurated_date', '>=', $startDate)->whereDate('inaugurated_date', '<=', $endDate)->get();
         return $data;
     }
+
+    public function getInauguratedSchool()
+    {
+        $data = School::select('id', 'school_name', 'inaugurated_date')->get();
+        return response()->json([
+            'success' => true,
+            'data' => $data
+        ]);
+    }
 }
